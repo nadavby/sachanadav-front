ead2d3312325757aaf711109cf0f32392d43a187
sachamak <sachachamak@gmail.com>
2025-03-10 17:28:54 +0200

    RegistrationForm+user-service

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.css'
+import App from './components/App.tsx'
+import { GoogleOAuthProvider } from '@react-oauth/google'
 
 createRoot(document.getElementById('root')!).render(
+  <GoogleOAuthProvider clientId="374573652102-chk83u6v9eq7bnu2prd9tkojuc9e8tu1.apps.googleusercontent.com">
   <StrictMode>
     <App />
   </StrictMode>,
+  </GoogleOAuthProvider>
 )
