4cc76121616f60a6f415140bcc9bd71938c91f9e
sachamak <sachachamak@gmail.com>
2025-03-11 15:15:35 +0200

    bggining of login page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,16 @@
+import {  Routes, Route } from "react-router-dom";
+import { Login } from "./Login";
 import { RegistrationForm } from "./RegristrationForm";
 
 function App() {
   
   return(
     <>
-    <RegistrationForm />
+   <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegistrationForm />} />
+   </Routes>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './components/App.tsx'
 import { GoogleOAuthProvider } from '@react-oauth/google'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <GoogleOAuthProvider clientId="374573652102-chk83u6v9eq7bnu2prd9tkojuc9e8tu1.apps.googleusercontent.com">
   <StrictMode>
-    <App />
+  <BrowserRouter>
+        <App />
+    </BrowserRouter>
   </StrictMode>,
   </GoogleOAuthProvider>
 )
