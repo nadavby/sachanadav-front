56d75c7ef9bc80e22139a7a241b84f3d57b25eb3
sachamak <sachachamak@gmail.com>
2025-03-19 11:41:54 +0200

    added some style and message error to the login page

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,6 +1,6 @@
 /** @format */
 
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 import userService from "../../services/user-service";
@@ -11,7 +11,12 @@ type FormData = {
 };
 
 export const Login: FC = () => {
-  const { register, handleSubmit } = useForm<FormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  const [serverError, setServerError] = useState("");
   const navigate = useNavigate();
 
   const onSubmit = async (data: FormData) => {
@@ -25,6 +30,7 @@ export const Login: FC = () => {
         navigate("/posts");
       }
     } catch (error) {
+      setServerError("Incorrect email or password.");
       console.error(error);
     }
   };
@@ -48,15 +54,31 @@ export const Login: FC = () => {
             padding: "20px",
             borderRadius: "10px",
           }}>
-          <h1 style={{ display: "flex", justifyContent: "center" }}>Login</h1>
-          <label>Email:</label>
+          <img
+            src="/favicon.jpg"
+            style={{
+              width: "100px",
+              height: "100px",
+              borderRadius: "20px",
+              display: "flex",
+            }}
+          />
+          <h1 style={{ display: "flex", justifyContent: "center" }}>
+            Boarding-pass
+          </h1>
+
+          <label>email:</label>
 
           <input
             type="email"
             className="mb-3"
             {...register("email")}
             required
+            style={{ borderRadius: "5px", border: "1px" }}
           />
+          {errors.email && (
+            <p className="text-danger">{errors.email.message}</p>
+          )}
 
           <label>password:</label>
           <input
@@ -64,9 +86,18 @@ export const Login: FC = () => {
             className="mb-3"
             {...register("password")}
             required
+            style={{ borderRadius: "5px", border: "1px" }}
           />
+          {errors.password && (
+            <p className="text-danger">{errors.password.message}</p>
+          )}
+          {serverError && <p className="text-danger">{serverError}</p>}
 
-          <button type="submit">Login</button>
+          <button
+            type="submit"
+            className="btn btn-outline-primary">
+            Login
+          </button>
           <p style={{ textAlign: "center" }}>
             <Link to="/register">Registration Form</Link>
           </p>
